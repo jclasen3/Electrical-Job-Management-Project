624a9236f50a132904a2ef05b8862c53710f3076
jclasen3 <120653296+jclasen3@users.noreply.github.com>
2024-11-25 06:51:03 -0600

    Update Job.java

--- Job.java
+++ Job.java
@@ -5,17 +5,32 @@ public class Job {
     private Client client;
     private String name;
     private Date dueDate;
+    private Worker assignedWorker;
 
     public Job(Client client, String name, Date dueDate) {
         this.client = client;
         this.name = name;
         this.dueDate = dueDate;
+        this.assignedWorker = null; // Initially unassigned
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void assignWorker(Worker worker) {
+        this.assignedWorker = worker;
+    }
+
+    public Worker getAssignedWorker() {
+        return assignedWorker;
     }
 
     @Override
     public String toString() {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        return "Job: " + name + ", Client: " + client.getName() + ", Due: " + sdf.format(dueDate);
+        String workerInfo = (assignedWorker != null) ? assignedWorker.getName() : "Unassigned";
+        return "Job: " + name + ", Client: " + client.getName() + ", Due: " + sdf.format(dueDate) + ", Worker: " + workerInfo;
     }
 }
 
