44d366c2513a9c513367c2c736d2ba58d5153d27
jclasen3 <120653296+jclasen3@users.noreply.github.com>
2024-11-26 06:29:57 -0600

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,16 +1,22 @@
 public class Client {
     private String name;
+    private String phoneNumber;
 
-    public Client(String name) {
+    public Client(String name, String phoneNumber) {
         this.name = name;
+        this.phoneNumber = phoneNumber;
     }
 
     public String getName() {
         return name;
     }
 
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
     @Override
     public String toString() {
-        return name;
+        return name + " (Phone: " + phoneNumber + ")";
     }
 }
